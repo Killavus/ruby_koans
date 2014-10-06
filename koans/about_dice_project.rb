6deba23c48dfa6715d4607a68db27aeea12e1529
Marcin Grzywaczewski <killavus@gmail.com>
2014-10-07 01:17:10 +0200

    Completed koans about dice project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  def roll(count)
+    @rolls = [].tap do |result|
+      count.times { result << [1,2,3,4,5,6].sample }
+    end
+  end
+
+  def values
+    @rolls
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
