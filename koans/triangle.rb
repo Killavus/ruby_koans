55424691917988e4796caa21d5c794e332bbd50d
Marcin Grzywaczewski <killavus@gmail.com>
2014-10-07 00:09:18 +0200

    Completed triangle project koans

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if all_sides_equal(a, b, c)
+  return :isosceles   if two_sides_equal(a, b, c)
+  :scalene
+end
+
+def all_sides_equal(a, b, c)
+  side_pairs(a, b, c).all? { |x, y| x == y }
+end
+
+def two_sides_equal(a, b, c)
+  side_pairs(a, b, c).any? { |x, y| x == y }
+end
+
+def side_pairs(a, b, c)
+  [a, b, c].combination(2)
 end
 
 # Error class used in part 2.  No need to change this code.
